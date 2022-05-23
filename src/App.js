e75bd9720fcbceee1731ed1fe9c32bcec9ec222a
Serhii <sergei181989@gmail.com>
2022-05-23 17:56:16 +0300

    :sparkles: Transferring the DB to api and displaying in the App component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react';
+
 import Sort from './components/Sort';
 import Header from './components/Header';
-import pizzas from './assets/pizzas.json';
 import Categories from './components/Categories';
 import PizzaBlock from './components/PizzaBlock';
+
 import './scss/app.scss';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('https://628b94df667aea3a3e326e2a.mockapi.io/items')
+      .then((res) => res.json())
+      .then((json) => {
+        setItems(json);
+      })
+  }, [])
+
+
+
   return (
     <div className="wrapper">
       <Header />
@@ -17,7 +31,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {[] && pizzas.map((obj) => (
+            {[] && items.map((obj) => (
               <PizzaBlock {...obj} key={obj.id} />
             ))}
           </div>
